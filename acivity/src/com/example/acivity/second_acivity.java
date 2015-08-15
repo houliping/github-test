3c856bb87160e3c5ed1776210afbe3d5da560fbd
Administrator <Administrator@QH-20150728JAZV>
2015-08-15 20:19:06 +0800

    第一次提交

---
+++ acivity/src/com/example/acivity/second_acivity.java
@@ -0,0 +1,14 @@
+package com.example.acivity;
+
+import android.os.Bundle;
+
+public class second_acivity extends MainActivity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.second_view);
+	}
+	
+
+}
